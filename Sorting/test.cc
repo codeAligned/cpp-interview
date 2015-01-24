cb83b0f747a1d67020a0dc98b09ec8cad9895ce4
Kevin Dungs <kevin@dun.gs>
2015-01-24 17:42:13 +0100

    Improve timing test, compare to std::sort.

--- Sorting/test.cc
+++ Sorting/test.cc
@@ -14,25 +14,28 @@ auto Print(const Container& c) -> void {
   std::cout << '\n';
 }
 
+template <typename Func>
+auto Time(const Func& f) -> unsigned {
+  const auto start = std::chrono::steady_clock::now();
+  f();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+             std::chrono::steady_clock::now() - start).count();
+}
+
 int main() {
   std::random_device rd;
   std::mt19937 rng(rd());
   std::uniform_real_distribution<double> dist(0.0, 1.0);
-
-  auto v = std::vector<double>(10000000);
-  auto fillRandom = [&]() {
+  auto fillRandom = [&](std::vector<double>& v) {
     std::generate(std::begin(v), std::end(v), [&]() { return dist(rd); });
   };
 
-  auto now = std::chrono::steady_clock::now;
+  auto v = std::vector<double>(10000000);
+  fillRandom(v);
+  auto w = v;
+  const auto timeStd = Time([&]() { std::sort(std::begin(v), std::end(v)); });
+  const auto timeMrg = Time([&]() { MergeSort(std::begin(w), std::end(w)); });
+  std::cout << "std::sort: " << timeStd << "ms\n";
+  std::cout << "MergeSort: " << timeMrg << "ms\n";
 
-  auto timedTest = [&]() {
-    fillRandom();
-    const auto start = now();
-    MergeSort(std::begin(v), std::end(v));
-    return std::chrono::duration_cast<std::chrono::milliseconds>(now() - start)
-        .count();
-  };
-
-  std::cout << timedTest() << "ms\n";
 }
